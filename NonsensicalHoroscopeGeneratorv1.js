c5aeba8447f0defe0a7779d81ecbc365a110d830
Sudy S <98613516+SudyS88@users.noreply.github.com>
2022-01-28 15:15:59 -0600

    Add files via upload

---
+++ NonsensicalHoroscopeGeneratorv1.js
@@ -0,0 +1,7 @@
+/* Nonsensical Horoscope Generator
+This tool will generate a randomized and quasi-nonsensical horoscope prediction. 
+1. It will first analyze the positions of the heavenly bodies. 
+2. It will then make a horoscope prediction
+3. Finally, it will give the user advice on how to mitigate the dire fate they have in store
+Enjoy!
+*/
